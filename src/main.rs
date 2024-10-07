5a1238dd50e751417eb171559af05d05c6266f45
Dimitar85898 <83722607+Dimitar85898@users.noreply.github.com>
2024-10-07 20:26:14 +0300

    [main] Parse args, load file and print size

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use std::{env, fs, process::ExitCode};
+
+fn main() -> ExitCode {
+    let mut args = env::args();
+    let mut input = String::new();
+    let _exe = args.next().expect("exe");
+    while let Some(arg) = args.next() {
+        if input.is_empty() { input = arg; }
+        else {
+            eprintln!("ERROR: Unknown argument `{}`",arg);
+            return ExitCode::FAILURE;
+        }
+    }
+    if input.is_empty() {
+        eprintln!("ERROR: Missing input file");
+        return ExitCode::FAILURE;
+    }
+    let bytes = match fs::read(&input) {
+        Err(e) => {
+            eprintln!("ERROR: Failed to read `{}`: {}", input, e);
+            return ExitCode::FAILURE;
+        }
+        Ok(v) => v,
+    };
+    println!("INFO: File size = {}", bytes.len());
+    ExitCode::SUCCESS
 }
